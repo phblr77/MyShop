2ad26392019b8c3a0ffe831b8545c160916e4808
krukd <krukd87@gmail.com>
2021-07-02 22:08:04 +0200

    Initial commit

---
+++ src/main/java/ru/geekbrains/shop/MyShopApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/shop/MyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
